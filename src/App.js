ec0d2a278a9318dae9995c27de873253b86fb478
Joe Shih <injoe1001@gmail.com>
2022-03-22 10:13:58 +0800

    Chart update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import RadarChart from "./components/RadarChart";
+import { UserData } from "./Data";
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.nutrients),
+    datasets: [
+      {
+        label: "鮭魚",
+        data: UserData.map((data) => data.userGain),
+        fill: true,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgb(255, 99, 132)",
+        pointBackgroundColor: "rgb(255, 99, 132)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgb(255, 99, 132)",
+      },
+    ],
+    
+  });
+ const [options, setOptions] = useState({
+  legend: {
+    position: 'top'
+  },
+  title: {
+    display: true,
+    text: 'Chart.js Radar Chart'
+  },
+  elements: {
+    line: {
+      borderWidth: 1,
+    },
+  },
+  scale: {
+    r:{
+      beginAtZero: true,
+      min:0,   
+      max:100,
+    },
+    // angleLines: {
+    //   color: "rgba(255, 255, 255, .3)",
+    //   lineWidth: 1
+    // },
+    // gridLines: {
+    //   color: "rgba(255, 255, 255, .3)",
+    //   circular: true
+    // },
+    ticks: {
+      min: 0,
+      max: 50,
+      stepSize: 10,
+      showLabelBackdrop: false,
+      backdropColor: "rgba(203, 197, 11, 1)"
+    }
+  },
+  
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ width: "500px" }}>
+        <RadarChart chartData={userData} options={options} />
+      </div>
+      {/* <div style={{width:"700px"}}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{width:"700px"}}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{width:"700px"}}>
+        <PieChart chartData={userData} />
+      </div> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const UserData = [
+    {
+        id:1,
+        nutrients: "SUGARS",
+        userGain: 50,
+        userLost: 823,
+    },
+    {
+        id:2,
+        nutrients: "FAT",
+        userGain: 100,
+        userLost: 222,
+    },
+    {
+        id:3,
+        nutrients: "PROTEIN",
+        userGain: 50,
+        userLost: 900,
+    },
+    {
+        id:4,
+        nutrients: "CALORIES",
+        userGain: 100,
+        userLost: 600,
+    },
+    {
+        id:5,
+        nutrients: "SODIUM",
+        userGain: 60,
+        userLost: 420,
+    },
+    {
+        id:6,
+        nutrients: "FIBER",
+        userGain: 100,
+        userLost: 420,
+    },
+]
